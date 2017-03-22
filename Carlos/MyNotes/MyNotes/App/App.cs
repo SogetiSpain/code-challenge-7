3ca3aa941cf8f411d9d4edaf98ccdc10b692dd45
Carlos <c.canizaresestevez@gmail.com>
2017-03-22 09:13:22 +0100

    v1

---
+++ Carlos/MyNotes/MyNotes/App/App.cs
@@ -0,0 +1,42 @@
+﻿using MyNotes.App;
+using MyNotes.Client;
+using MyNotes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.App
+{
+    public class App
+    {
+        private readonly IClient<Note> _notes;
+        private readonly ICommandService _commandService;
+        private readonly ICollectionService<Note> _notesCollection;
+
+        public App()
+        {
+            //Firebase Info:
+            var resourceUrl = "https://mynotes-87fd2.firebaseio.com/";
+            var securityToken = "XUCrsUoS3lxsQlYMcsBuIhGI21OH1N5iYM3sJCUD";
+            _notes = new FirebaseClient<Note>(resourceUrl,securityToken);
+
+            //If you want to switch to another client you have to set here the 
+
+            _notesCollection = new NoteCollectionService(_notes);
+            _commandService = new CommandService(_notesCollection);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to MyNotes");
+            while (true)
+            {
+                Console.WriteLine("Avaible commands: notes push text | notes remove id | notes update id text | notes get text | notes get id | exit");
+                var command = Console.ReadLine();
+                _commandService.HandleCommand(command);
+            }            
+        }
+    }
+}
---
+++ Carlos/MyNotes/MyNotes/Program.cs
@@ -0,0 +1,48 @@
+﻿using MyNotes.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Newtonsoft.Json.Linq;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp;
+using MyNotes.App;
+
+namespace MyNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test().Wait();
+            new App.App().Start();
+        }
+
+        static async Task Test()
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "XUCrsUoS3lxsQlYMcsBuIhGI21OH1N5iYM3sJCUD",
+                BasePath = "https://mynotes-87fd2.firebaseio.com/"
+            };
+
+            IFirebaseClient client = new FirebaseClient(config);
+            var testNote = new Note()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Date = DateTime.Now,
+                Tags = new List<string>() { "gulp", "front", "sass", "minify" },
+                Text = "Set up gulp task in javascript projects to improve quality in front end architechture"
+            };
+
+            await client.PushAsync<Note>($"notes/{testNote.Id}", testNote);
+            
+        }
+
+    }
+}
